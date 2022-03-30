0c07c65fdde8abce6a0de9cff3b48ad8c47c4e4b
reginaldouemura <reginaldo.uemura@gmail.com>
2022-03-30 16:58:16 -0300

    react01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Item from "./components/Item";
+import Card from "./components/Card";
+
+
+const App = () => {
+  return(
+    <>
+    <h1>Minha primeira aplicação com react</h1>
+    <ul>
+     <Item>
+       Item1
+       </Item>
+     <Item>
+       Item2
+       </Item>
+     <Item>
+       item3
+       </Item>
+    </ul>
+    <Card/>
+    </>
+
+  )
+    
+  }
 
 export default App;
